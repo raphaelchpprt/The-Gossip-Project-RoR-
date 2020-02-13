d099dbe0accf65994df292e60300c57edf369b80
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-02-13 19:36:44 +0100

    likes in progess

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -3,6 +3,7 @@ class Gossip < ApplicationRecord
   has_many :gossip_x_tags
   has_many :tags, through: :gossip_x_tags
   has_many :comments
+  has_many :likes
   validates :title, 
     presence: true,
     length: { in: 3..14 }
