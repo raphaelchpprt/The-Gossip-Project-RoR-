88c2302673d356b26caab6737259c674d8660f8a
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-02-13 11:24:05 +0100

    comments modification in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,12 +10,24 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.new
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(content: params[:content])
+      redirect_to(gossip_path(params[:gossip_id]), notice: 'Comment modified!')
+    end
   end
 
   def destroy
+    @gossip = Gossip.find(params[:gossip_id])
     @comment = Comment.find(params[:id])
     @comment.destroy
+    redirect_to(gossip_path(params[:gossip_id]), notice: 'Comment destroyed!')
   end
+
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
-	belongs_to :gossip
+  belongs_to :gossip
+  validates :content, 
+    presence: true,
+    allow_blank: false
 end
